1b03075702b38d6d3144e0e8b025bb766e1ff522
isaacsila <99277610+isaacsila@users.noreply.github.com>
2022-03-12 13:29:11 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
-
- * main - Entry point
-
- * Description: Print lower case a-z
-
- * Return: Return
-
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
-
 int main(void)
-
 {
+	char c = 'z';
 
-	char ch;
-
-
-
-	for (ch = 'z'; ch >= 'a'; ch--)
-
-		putchar(ch);
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
 
 	putchar('\n');
-
 	return (0);
-
 }
