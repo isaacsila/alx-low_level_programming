59188b54000ae2a2600a0e06cb78ffafd151dbb0
isaacsila <isaacsila431@gmail.com>
2022-04-26 01:55:30 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,19 +1,33 @@
 #include "lists.h"
-/**
- *print_listint_safe - prints a listint_t linked list
- *@head: pointer to the first element
- *Return: number of nodes on a list
- */
 
+/**
+ *print_listint_safe - printf a list of elements with address
+ *@head: head of the linked list
+ *
+ *Return: size of the linked list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-  size_t temp;
-  unasigned int cout = 1;
+	size_t n;
+	int *add_a, *add_b;
 
-  temp = head;
-  while (temp != NULL)
-    {
-      cout << temp->n << "";
-      temp = temp->next;
-    }
+	n = 0;
+	if (head == NULL)
+		return (n);
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_a = (int *)&head;
+		add_b = (int *)&head->next;
+		if (add_a[0] - add_b[0] <= 0)
+		{
+			head = head->next;
+			n++;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			return (n);
+		}
+		head = head->next;
+		n++;
+	}
+	return (n);
 }
