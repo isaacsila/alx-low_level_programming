77c7efa45df4e2f347862248050e9c1c9d166cc3
isaacsila <99277610+isaacsila@users.noreply.github.com>
2022-07-03 05:36:43 +0300

    first task done

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ *  * _puts - prints string
+ *   * @str: input string
+ *    * Return: no return
+ *     */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+
+/**
+ *  * _abs - check the code for main School students.
+ *   *
+ *    * @n: is an integer argument
+ *     *
+ *      * Return: Always 0.
+ *       */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
