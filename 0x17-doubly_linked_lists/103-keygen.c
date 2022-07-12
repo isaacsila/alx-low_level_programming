38889b660152716314b49caed74e493b23a01c64
isaacsila <99277610+isaacsila@users.noreply.github.com>
2022-07-12 09:57:18 +0300

    fuck u

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,36 +1,58 @@
-#include "lists.h"
+/*
+ * File: 103-keygen.c
+ * Auth: Isaac Sila
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- * @head: A pointer to the head of the dlistint_t list.
- * @n: The integer for the new node to contain.
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * Return: Always 0.
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	dlistint_t *new, *tail;
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	new->n = n;
-	new->next = NULL;
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-	if (*head == NULL)
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
+
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		new->prev = NULL;
-		*head = new;
-		return (new);
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-	tail = *head;
-	while (tail->next != NULL)
-		tail = tail->next;
-	tail->next = new;
-	new->prev = tail;
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-	return (new);
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
